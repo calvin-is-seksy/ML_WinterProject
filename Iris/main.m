d71cb45ed7993d2824d40d99014e93fb54d275b5
Calvin Wang <calvin.wang0628@gmail.com>
2017-01-17 00:35:36 -0800

    renamed directories

--- Iris/main.m
+++ Iris/main.m
@@ -36,7 +36,7 @@ X = [ones(m, 1), petal_L]; % Add a column of ones to x
 theta = zeros(2, 1); % initialize fitting parameters
 
 % Some gradient descent settings
-iterations = 1500;
+iterations = 15000;
 alpha = 0.01;
 
 % compute and display initial cost
@@ -53,7 +53,43 @@ hold off;
 
 % predict values for petal lengths 3 cm and 6 cm 
 predict1 = [1, 3] * theta; 
+fprintf('For a petal length of 3 cm, we predict a petal width of %f cm.\n',... 
+    predict1); 
 
+predict2 = [1, 6] * theta; 
+fprintf('For a petal length of 6 cm, we predict a petal width of %f cm.\n',... 
+    predict2); 
 
+% visualizing J(theta_0, theta_1) 
+fprintf('Visualizing J(theta_0, theta_1) ...\n') 
 
+% Grid over which I will calculate J 
+theta0_vals = linspace(-10, 10, 100);
+theta1_vals = linspace(-1, 4, 100);
+
+J_vals = zeros(length(theta0_vals), length(theta1_vals));
+
+% Fill out J_vals
+for i = 1:length(theta0_vals)
+    for j = 1:length(theta1_vals)
+	  t = [theta0_vals(i); theta1_vals(j)];    
+	  J_vals(i,j) = computeCost(X, sepal_W, t);
+    end
+end
+
+% Because of the way meshgrids work in the surf command, we need to 
+% transpose J_vals before calling surf, or else the axes will be flipped
+J_vals = J_vals';
+% Surface plot
+figure;
+surf(theta0_vals, theta1_vals, J_vals)
+xlabel('\theta_0'); ylabel('\theta_1');
+
+% Contour plot
+figure;
+% Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
+contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
+xlabel('\theta_0'); ylabel('\theta_1');
+hold on;
+plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
 
